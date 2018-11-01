bffe31cce304d54b6b63f00c8fd2db8eb9b2d67e
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2018-11-01 23:22:07 +0900

    Add validations for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_4byte_char
+
+  private
+
+  def validate_name_not_including_4byte_char
+    if name&.chars.any? {|c| c.bytesize >= 4 }
+      errors.add(:name, "に特殊な文字を含めることはできません。")
+    end
+  end
 end
