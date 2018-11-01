63877ab3d22cbc19ff84808f433deb573d6f5f98
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2018-11-01 23:24:02 +0900

    Set automatically name for unnamed task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_4byte_char
 
@@ -8,5 +10,9 @@ class Task < ApplicationRecord
     if name&.chars.any? {|c| c.bytesize >= 4 }
       errors.add(:name, "に特殊な文字を含めることはできません。")
     end
+  end
+
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
   end
 end
