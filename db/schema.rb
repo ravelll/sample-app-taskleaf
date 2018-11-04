7ac46ba70fb8fabf9ee6def9ac9d5b1a990786ee
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2018-11-04 22:17:54 +0900

    Add relation between user and tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_142953) do
+ActiveRecord::Schema.define(version: 2018_11_04_122959) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
