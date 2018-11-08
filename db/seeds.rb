4f416ef5ea3024f01f4907cc7474e67795bc538b
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2018-11-08 18:11:59 +0900

    Add seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.create({
+  name: 'Taniguchi',
+  email: 'taniguchi@example.com',
+  password: 'password',
+  password_confirmation: 'password',
+})
+
+Task.create([
+  {
+    name: 'タスクを作る',
+    description: 'やっていく必要があるのでタスクを作ります',
+    user: u
+  },
+  {
+    name: 'より凄いタスクを作る',
+    description: '人間は日々進化するのでタスクの難易度を徐々に高めることで自己効果感が維持されます',
+    user: u
+  },
+])
